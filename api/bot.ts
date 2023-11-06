4ee59684dc1f266becfa75f146f5e8cfe0a69ab6
ferric <ferric-sol@ferric.net>
2023-11-05 22:55:11 -0600

    Log returnUrl

--- api/bot.ts
+++ api/bot.ts
@@ -115,6 +115,7 @@ menu.dynamic(async () => {
   // const appUrl = `${process.env.VERCEL_URL}`;
   const appUrl = 'https://zupass.org'
   const returnUrl = `${process.env.VERCEL_URL}/api/zucheck`;
+  console.log('returnUrl: ', returnUrl);
   let proofUrl = await constructZupassPcdGetRequestUrl(appUrl, returnUrl, ZKEdDSAEventTicketPCDPackage.name, args, {
     genericProveScreen: true,
     title: "",

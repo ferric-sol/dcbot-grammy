5ea14826f786aad64f304ac15d82b5c6e8dad086
Rohan <RohanZarathustra@gmail.com>
2023-11-07 11:14:21 -0500

    drip or drown

--- api/zucheck.ts
+++ api/zucheck.ts
@@ -1,21 +1,36 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 import {
   ZKEdDSAEventTicketPCD,
-  ZKEdDSAEventTicketPCDPackage
+  ZKEdDSAEventTicketPCDPackage,
 } from "@pcd/zk-eddsa-event-ticket-pcd";
 import { getEdDSAPublicKey } from "@pcd/eddsa-pcd";
-import { createClient } from '@vercel/kv';
+import { createClient } from "@vercel/kv";
 import { Bot, webhookCallback } from "grammy";
+import { ethers } from "ethers";
 
-const { KV_REST_API_URL, KV_REST_API_TOKEN, GNOSIS_URL, TELEGRAM_API_KEY } = process.env;
+const {
+  KV_REST_API_URL,
+  KV_REST_API_TOKEN,
+  GNOSIS_URL,
+  TELEGRAM_API_KEY,
+  ALCHEMY_GOERLI_KEY,
+  FAUCET_PRIVATE_KEY,
+} = process.env;
 
-const token = process.env.TELEGRAM_API_KEY
+const token = process.env.TELEGRAM_API_KEY;
 if (!token) throw new Error("BOT_TOKEN is unset");
 
 const bot = new Bot(token);
 
-if (!KV_REST_API_URL || !KV_REST_API_TOKEN || !GNOSIS_URL || !TELEGRAM_API_KEY) {
-  throw new Error('Environment variables KV_REST_API_URL and KV_REST_API_TOKEN and ALCHEMY_URL and TELEGRAM_API_KEY must be defined');
+if (
+  !KV_REST_API_URL ||
+  !KV_REST_API_TOKEN ||
+  !GNOSIS_URL ||
+  !TELEGRAM_API_KEY
+) {
+  throw new Error(
+    "Environment variables KV_REST_API_URL and KV_REST_API_TOKEN and ALCHEMY_URL and TELEGRAM_API_KEY must be defined"
+  );
 }
 
 export const closeWebviewHtml = `
@@ -56,7 +71,9 @@ async function verifyZKEdDSAEventTicketPCD(
 
   let signerMatch = false;
 
-  const zupassPubkeyReq = await fetch('https://api.zupass.org/issue/eddsa-public-key');
+  const zupassPubkeyReq = await fetch(
+    "https://api.zupass.org/issue/eddsa-public-key"
+  );
   const TICKETING_PUBKEY = await zupassPubkeyReq.json();
 
   signerMatch =
@@ -73,17 +90,22 @@ async function verifyZKEdDSAEventTicketPCD(
     console.log("[TELEGRAM] pcd invalid");
     return null;
   }
+}
 
-};
- 
 export async function GET(request: Request, res: Response) {
   const { searchParams } = new URL(request.url);
   try {
-    const proof = searchParams.has('proof') ? searchParams.get('proof') : null;
-    const telegram_user_id = searchParams.has('') ? searchParams.get('id') : null;
-    const chat_id = searchParams.has('telegram_chat_id') ? searchParams.get('telegram_chat_id') || 0 : 0;
-    let telegram_username = searchParams.has('username') ? searchParams.get('username') : undefined;
-    console.log('telegram_username: ', telegram_username, searchParams);
+    const proof = searchParams.has("proof") ? searchParams.get("proof") : null;
+    const telegram_user_id = searchParams.has("")
+      ? searchParams.get("id")
+      : null;
+    const chat_id = searchParams.has("telegram_chat_id")
+      ? searchParams.get("telegram_chat_id") || 0
+      : 0;
+    let telegram_username = searchParams.has("username")
+      ? searchParams.get("username")
+      : undefined;
+    console.log("telegram_username: ", telegram_username, searchParams);
 
     // if (!telegram_user_id || !/^-?\d+$/.test(telegram_user_id)) {
     //   throw new Error(
@@ -101,13 +123,13 @@ export async function GET(request: Request, res: Response) {
         (telegram_username && ` with username ${telegram_username}`)
     );
 
-    const pcd = await verifyZKEdDSAEventTicketPCD(proof ?? '');
+    const pcd = await verifyZKEdDSAEventTicketPCD(proof ?? "");
 
-    if(pcd && telegram_username) {
+    if (pcd && telegram_username) {
       // User verified, give access and close modal
       const kv = createClient({
-        url: KV_REST_API_URL || '',
-        token: KV_REST_API_TOKEN || '',
+        url: KV_REST_API_URL || "",
+        token: KV_REST_API_TOKEN || "",
       });
 
       const { watermark } = pcd.claim;
@@ -119,14 +141,36 @@ export async function GET(request: Request, res: Response) {
 
       // Check if current time is 10 minutes after claim time
       if (Date.now() - claimTimestamp >= TEN_MINUTES_IN_MS) {
-        console.log('Date.now() is 10 minutes after pcd.claim');
+        console.log("Date.now() is 10 minutes after pcd.claim");
       } else {
-        console.log('Date.now() is not yet 10 minutes after pcd.claim');
+        console.log("Date.now() is not yet 10 minutes after pcd.claim");
       }
 
       await kv.set(`verified_user:${telegram_username}`, Date.now());
-      await bot.api.sendMessage(chat_id, 'You have successfully verified, type /start to play')
+      await bot.api.sendMessage(
+        chat_id,
+        "You have successfully verified, type /start to play"
+      );
       // TODO: Drip funds from the fauucet to this user's address
+      const FAUCET_AMOUNT = ".01";
+
+      // Connect to provider
+      const goerliProvider = new ethers.providers.AlchemyProvider(
+        5,
+        ALCHEMY_GOERLI_KEY
+      );
+
+      // Connect to our Faucet EOA
+      const goerliWallet = new ethers.Wallet(
+        wallet_private_key,
+        goerliProvider
+      );
+
+      // Send the funds
+      const txGoerli = await goerliWallet.sendTransaction({
+        to: address,
+        value: ethers.utils.parseEther(FAUCET_AMOUNT),
+      });
     }
 
     console.log(
@@ -144,7 +188,7 @@ export async function GET(request: Request, res: Response) {
   return new NextResponse(closeWebviewHtml, {
     status: 200,
     headers: {
-      'Content-Type': 'text/html'
+      "Content-Type": "text/html",
     },
   });
 }

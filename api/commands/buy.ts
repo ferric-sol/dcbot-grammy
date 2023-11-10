b6bd1cd6b341c22b87021e5cf308d2aa875d2c83
Rohan <RohanZarathustra@gmail.com>
2023-11-10 15:21:12 -0500

    console logging

--- api/commands/buy.ts
+++ api/commands/buy.ts
@@ -54,7 +54,7 @@ export default async function buy(tokenName: string, amount: number) {
   console.log("price:", price);
 
   // Use `price` to calculate min value out
-  const salt = amount * price;
+  const salt = BigInt(amount) * BigInt(price);
   console.log("salt in:", salt);
 
   // Calculate minimum fruit token amount to receive

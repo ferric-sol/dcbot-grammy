9f4eae3a1a2a928e30a9b12292fa19057986dd15
ferric <ferric-sol@ferric.net>
2023-11-10 13:11:29 -0600

    Catch errors so bot doesn't 500 out and go into infinite bot loop land

--- api/commands/price.ts
+++ api/commands/price.ts
@@ -4,37 +4,44 @@ import { gnosis } from "viem/chains";
 import { contracts } from "../contracts"
 
 export default async function getPrice(tokenName: string) {
-  // Get the faucet EOA account
-  if(!process.env.FRUITBOT_FAUCET_KEY) return false;
+  try {
+    // Get the faucet EOA account
+    if(!process.env.FRUITBOT_FAUCET_KEY) return false;
 
-  const account = privateKeyToAccount(
-    `0x${process.env.FRUITBOT_FAUCET_KEY}`
-  );
-  const dexContractName: string = `BasicDex${tokenName}`;
+    const account = privateKeyToAccount(
+      `0x${process.env.FRUITBOT_FAUCET_KEY}`
+    );
+    const dexContractName: string = `BasicDex${tokenName}`;
+    console.log('dexContractName: ', dexContractName)
 
-  // Initialize the viem client
-  const client = createWalletClient({
-    account,
-    chain: gnosis,
-    transport: http(process.env.GNOSIS_URL),
-  }).extend(publicActions);
+    // Initialize the viem client
+    const client = createWalletClient({
+      account,
+      chain: gnosis,
+      transport: http(process.env.GNOSIS_URL),
+    }).extend(publicActions);
 
-  // TODO: TSify this using types from
-  // https://github.com/BuidlGuidl/event-wallet/blob/08790b0d8f070b22625b1fadcd312988a70be825/packages/nextjs/utils/scaffold-eth/contract.ts#L7 
-  const tokenContract = (contracts as any)[`${dexContractName}`];
+    // TODO: TSify this using types from
+    // https://github.com/BuidlGuidl/event-wallet/blob/08790b0d8f070b22625b1fadcd312988a70be825/packages/nextjs/utils/scaffold-eth/contract.ts#L7 
+    const tokenContract = (contracts as any)[`${dexContractName}`];
+    console.log('tokenContract: ', tokenContract)
 
-  if (!tokenContract) {
-    throw new Error(`Token ${tokenName} not found in contracts`);
+    if (!tokenContract) {
+      throw new Error(`Token ${tokenName} not found in contracts`);
+    }
+    
+    const data = await client.readContract({
+      address: tokenContract.address,
+      abi: tokenContract.abi,
+      functionName: "creditInPrice",
+      args: [parseEther("1")]
+    });
+
+    console.log("data:", data.toString());
+    console.log("data2:", formatEther(data));
+    return `Price of 1 ${tokenName} is: ${formatEther(data)}`;
+  } catch (e) {
+    console.log('Error: ', (e as Error).message);
+    return false;
   }
-  
-  const data = await client.readContract({
-    address: tokenContract.address,
-    abi: tokenContract.abi,
-    functionName: "creditInPrice",
-    args: [parseEther("1")]
-  });
-
-  console.log("data:", data.toString());
-  console.log("data2:", formatEther(data));
-  return `Price of 1 ${tokenName} is: ${formatEther(data)}`;
 }

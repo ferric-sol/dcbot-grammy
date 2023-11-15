12694b16ab01e72bbca55d2c2efd19c5ecda85d7
ferric <ferric-sol@ferric.net>
2023-11-15 15:39:23 -0600

    Add validEventIds for devconnect

--- api/commands/start.ts
+++ api/commands/start.ts
@@ -10,6 +10,19 @@ import { Menu, MenuRange } from "@grammyjs/menu";
 import { ArgumentTypeName } from "@pcd/pcd-types";
 import { SemaphoreIdentityPCDPackage } from "@pcd/semaphore-identity-pcd";
 
+export const DEVCONNECT_VALID_EVENT_IDS = [
+  "785e8a0e-6734-11ee-b810-a2b83754f6bc", // AW Assembly
+  "0996f5fa-6736-11ee-a3bd-a2b83754f6bc", // Programmable Cryptography
+  "f626d630-2f8a-11ee-be83-b2dd9fd377ba", // 0xPARC Event
+  "a1c822c4-60bd-11ee-8732-763dbf30819c", // Devconnect Cowork Space
+  "3049870c-6cc8-11ee-98f3-7ebd6aca95cd", // Solidity Summit
+  "aebcb892-69e5-11ee-b65e-a2b83754f6bc", // EVM Summit
+  "7b57a8fc-6bae-11ee-bf2a-9e102a509962", // ETHconomics
+  "e1423686-6cc7-11ee-98f3-7ebd6aca95cd", // Next Billion
+  "140b208c-6d1d-11ee-8320-126a2f5f3c5e", // Wallet Unconference
+   "b03bca82-2d63-11ee-9929-0e084c48e15f" // TEST event
+];
+
 export function zupass_menu () { 
   // Initialize zupass menu
   const menu = new Menu("zupass");
@@ -62,7 +75,7 @@ export function zupass_menu () {
     },
     validEventIds: {
       argumentType: ArgumentTypeName.StringArray,
-      value: ["b03bca82-2d63-11ee-9929-0e084c48e15f"],
+      value: DEVCONNECT_VALID_EVENT_IDS,
       userProvided: false,
     },
     watermark: {
